062fff26e220e2b84ee12611998959201ac1cee1
Rafael Santos <rafael.ligue@outlook.com>
2025-06-24 11:18:00 -0300

    chore: add shadcn setup

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+const Home = () => {
+  return (
+    <div className="text-2xl font-bold text-blue-500">
+      Home - Dashboard
+      <Button>Clique Aqui</Button>
+    </div>
+  );
+};
+
+export default Home;
