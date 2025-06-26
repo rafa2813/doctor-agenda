a40fbe2b99898686920b0edd01fb8393e6f5c543
Rafael Santos <rafael.ligue@outlook.com>
2025-06-26 18:54:23 -0300

    feat: add clinic form page

---
+++ src/app/clinic-form/page.tsx
@@ -0,0 +1,31 @@
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+
+import ClinicForm from "./components/form";
+
+const ClinicFormPage = () => {
+  return (
+    <div>
+      <Dialog open>
+        <form>
+          <DialogContent className="sm:max-w-[425px]">
+            <DialogHeader>
+              <DialogTitle>Cadastro de clínica</DialogTitle>
+              <DialogDescription>
+                Preencha os campos abaixo para cadastrar sua clínica.
+              </DialogDescription>
+            </DialogHeader>
+            <ClinicForm />
+          </DialogContent>
+        </form>
+      </Dialog>
+    </div>
+  );
+};
+
+export default ClinicFormPage;
